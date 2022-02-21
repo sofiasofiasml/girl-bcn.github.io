4db6a3c8587d2bcfd6219152368731b1dd359975
sophie <romata1966@gmail.com>
2022-02-21 21:58:51 +0100

    funcion de ordenar lista

--- public/logic.js
+++ public/logic.js
@@ -29,6 +29,21 @@ var LOGIC = {
             li.appendChild(VotacionesNew); 
             CORE.navUl.appendChild(li); 
         }
+    }, 
+    //Ordenar lista en funcion de la fecha
+    // https://es.stackoverflow.com/questions/259945/ordenar-ul-javascript
+    ordenarLista: function(idUl){
+        //Obtenemos el elemento ul
+        let ul = document.getElementById(idUl);    
+        //Obtenemos la lista de li
+        let lista = ul.getElementsByTagName("li");
+        //Creamos el array a partir de los elementos li
+        //A continuación ordenamos con sort (hay que ordenar mirando el textContent y evitando la etiqueta li
+        //Por último recorremos el array ya ordenado y vamos haciendo el append en el elemento ul (sobrescribiéndolo)
+        let arrayCanciones = Array.from(lista);
+        arrayCanciones.sort((a, b) => a.textContent.localeCompare(b.textContent))
+        .forEach(li => ul.appendChild(li));
+       
     }
 
 
